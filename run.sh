b21bde8d3721155e1c56865de5a35357067c6415
jfsenechal <jfsenechal@gmail.com>
2025-11-07 13:58:45 +0100

    up

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
 npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
+
+```bash
+npx @tailwindcss/cli -i wp-content/themes/marchebe/assets/css/tailwind.css -o wp-content/themes/marchebe/assets/css/marchebe.css --watch
+```
+
+
+<a href="https://www.facebook.com/adlmarche"><img class="alignnone wp-image-49" src="https://adl.marche.be/wp-content/uploads/2023/07/brand-facebook.png" alt="Facebook" width="40" height="40" /><img class="alignnone wp-image-50" src="https://adl.marche.be/wp-content/uploads/2023/07/brand-instagram.png" alt="" width="40" height="40" /></a>
